cd9d66a10858608c7646892dceadd673c6cf3a06
abhinavkwj <75260515+abhinavkwj@users.noreply.github.com>
2021-06-11 13:47:14 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,60 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    background("orange");
+    
+  question.hide()
+    textSize(20);
+    text("result of the quiz",340,50);
+    text("____________________",320,65);
+    Contestant.getPlayerInfo();
+  if (allContestants!==undefined){
+    fill("blue");
+    textSize(20);
+    text("*Note: Contestant who answered correct are highlighted in green colour!",130,230);
+    var displayAnswer=230;
+    for(var plr in allContestants){
+      var correctAnswer="2";
+      if(correctAnswer==allContestants[plr].answer){
+        fill("green")
+      }
+      else{
+        fill("red");
+      }
+      displayAnswer+=30
+      text(allContestants[plr].name+": "+allContestants[plr].answer,250,displayAnswer)
+    }
+  }
+
+   
+  }
+
+}
